4194abdfc15add3b35d8f7f0035c878bd53908e9
its-sachin <sachinsain28786@gmail.com>
2021-10-17 19:38:50 +0530

    ns file added

---
+++ A3/ns-allinone-3.29/ns-3.29/build/ns3/config-store-config.h
@@ -0,0 +1,11 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_NS3_CONFIG_STORE_CONFIG_H_WAF
+#define W_NS3_CONFIG_STORE_CONFIG_H_WAF
+
+#define PYTHONDIR "/usr/local/lib/python3/dist-packages"
+#define PYTHONARCHDIR "/usr/local/lib/python3/dist-packages"
+/* #undef HAVE_PYEMBED */
+#define HAVE_GTK 1
+
+#endif /* W_NS3_CONFIG_STORE_CONFIG_H_WAF */
